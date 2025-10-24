4c764e4dbcfed82a4eedaf827d762871a7d597ed
Jacob Rennemark <jacobrane.se@gmail.com>
2025-10-24 12:36:57 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+const API_URL = 'http://localhost:5000'
+
+export default function App() {
+  const [tasks, setTasks ] = useState([]);
+
+  async function loadTasks() {
+    const response = await fetch(`${API_URL}/api/tasks`);
+    const data = await response.json();
+    setTasks(data);
+  }
+  useEffect(() => { loadTasks() }, [])
+
+  return (
+    <>
+     <main>
+      <h1>TODO App</h1>
+
+      <ul>
+        {tasks.map((t) => (
+          <li key={t.id}>
+            {t.title} - {t.status}
+          </li>
+        ))}
+      </ul>
+     </main>
+    </>
+  )
+};
+
